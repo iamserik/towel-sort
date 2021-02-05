d5f00ddf51b87b01cc3bce8e5a759e916b7c4073
Serik <serj_navii@mail.ru>
2021-02-05 10:54:32 +0600

    init: write towelString function for task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+  if(arguments.length !== 0 && matrix.length !== 0){ 
+    for(let i=0; i<matrix.length; i++){
+      if(i%2 !== 0) res = res.concat(matrix[i].sort((a, b) => b - a));
+      else res = res.concat(matrix[i].sort((a, b) => a - b))  
+    }
+  }
+  return res;
 }
