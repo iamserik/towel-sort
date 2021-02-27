bf8f744b317a875d69b031e7f3d50b34dc902da0
Serik <serj_navii@mail.ru>
2021-02-27 10:32:39 +0600

    refactor: change structure of function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let res = [];
-  if(arguments.length !== 0 && matrix.length !== 0){ 
-    for(let i=0; i<matrix.length; i++){
-      if(i%2 !== 0) res = res.concat(matrix[i].sort((a, b) => b - a));
-      else res = res.concat(matrix[i].sort((a, b) => a - b))  
-    }
-  }
-  return res;
+  return matrix !== undefined ? matrix.reduce((a, b, index) => a.concat((index % 2) === 0 ? b : b.reverse()), []) : [];
 }
